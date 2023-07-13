9f56528a369f26f9caafcc1c5da40a0630364621
Ricardo Ferreira <rnhc1000@gmail.com>
2023-07-13 14:27:09 -0300

    Day Two Task

---
+++ dayTwo/PrimitiveTypes.java
@@ -0,0 +1,30 @@
+package dayTwo;
+
+public class PrimitiveTypes {
+
+    public static void main(String[] args) {
+
+        byte b = 127;
+        char c = 'c';
+        short r = 32767;
+        int i = 65536;
+        long l = 1L;
+        float f = 1.0f;
+        double d = 1000d;
+        boolean t = true;
+
+        System.out.println(
+                b + " " +
+                c + " " +
+                r + " " +
+                i + " " +
+                l + " " +
+                f + " " +
+                d + " " +
+                t + " "
+
+        );
+
+    }
+
+}
