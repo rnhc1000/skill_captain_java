9e6d34ac77f6fed580fc83368154dbc3b0dc9d7e
Ricardo Ferreira <rnhc1000@gmail.com>
2023-07-13 14:59:24 -0300

    Day Four Task

---
+++ dayFour/LargerNumber.java
@@ -0,0 +1,24 @@
+package dayFour;
+/**
+ * Write a Java program that takes in two integer values from the user 
+ * and outputs the larger of the two values using a conditional statement.
+ */
+import java.util.Scanner;
+
+public class LargerNumber {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input the first number -> ");
+        int numberOne = input.nextInt();
+        System.out.print("Input the second number -> ");
+        int numberTwo = input.nextInt();
+        int largerNumber = (numberOne > numberTwo) ? numberOne : numberTwo;
+        input.close();
+        System.out.println("Number " + largerNumber + " is the largest");
+
+        
+    }
+    
+}
