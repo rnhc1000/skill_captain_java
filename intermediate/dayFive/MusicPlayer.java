54369558c22dad7888a479e7561914389d86f982
Ricardo Ferreira <rnhc1000@gmail.com>
2023-07-15 22:34:13 -0300

    All methods shall return a boolean

--- intermediate/dayFive/MusicPlayer.java
+++ intermediate/dayFive/MusicPlayer.java
@@ -1,5 +1,12 @@
 package intermediate.dayFive;
-
+/**
+ * 
+ * All methods shall return a boolean as:
+ * true: success
+ * false: fail
+ * @return boolean
+ * 
+ */
 public interface MusicPlayer {
 
         public boolean play();
