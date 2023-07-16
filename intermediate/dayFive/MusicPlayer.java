cfd0023a2846957e49b3719afc50f9f8d39de638
Ricardo Ferreira <rnhc1000@gmail.com>
2023-07-15 22:31:39 -0300

    Day Five Task in Progress...

---
+++ intermediate/dayFive/MusicPlayer.java
@@ -0,0 +1,11 @@
+package intermediate.dayFive;
+
+public interface MusicPlayer {
+
+        public boolean play();
+        public boolean pause();
+        public boolean stop();
+        public boolean addSong(String song);
+        public boolean removeSong(String song);
+    
+}
