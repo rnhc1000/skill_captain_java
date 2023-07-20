9a7226ee1ff917eb43523e935357bf2d5a65c4ec
Ricardo Ferreira <rnhc1000@gmail.com>
2023-07-20 11:05:10 -0300

    Day Six Task mySQL

---
+++ mySQL/daySix/truncateDrop.sql
@@ -0,0 +1,11 @@
+/**
+Assignments:
+
+Write a SQL query to drop a table named employees.
+Write a SQL query to truncate a table named employees.
+
+*/
+
+drop table Employees;
+
+truncate table Employees;
