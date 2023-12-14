7315d047bdd64b17f9151aa312a65741ddecc126
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-12-14 19:00:59 -0300

    Logger logger

--- advanced/dayFour/FutureFactorial/src/main/java/br/dev/ferreiras/App.java
+++ advanced/dayFour/FutureFactorial/src/main/java/br/dev/ferreiras/App.java
@@ -1,6 +1,7 @@
 package br.dev.ferreiras;
 
-// import java.lang.System.Logger.Level;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
@@ -9,10 +10,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.logging.Logger;
 
-/**
- * Hello world!
- *
- */
+
 public class App {
 
     private static final ExecutorService threadPool = Executors.newFixedThreadPool(10);
@@ -20,64 +18,69 @@ public class App {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
 
         Scanner input = new Scanner(System.in);
+        String logTimeDate = "dd.MM.yyyy HH:mm:ss.SSS";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(logTimeDate);
+        System.out.println(formatter.format(LocalDateTime.now()));
         System.out.print("Input the number to be processed: ");
-        int number = input.nextInt();
+        Long number = input.nextLong();
 
         Factorial numberToBeFactored = new Factorial(number);
-        System.out.println("Task had just been submitted to...");
+        Logger logger = Logger.getLogger(App.class.getName());
+        logger.info("Task had just been submitted!");
+        long start = System.currentTimeMillis();
         Future<Long> future = threadPool.submit(numberToBeFactored);
 
         while (!future.isDone()) {
-            System.out.println("Task being processed...Wait!");
+            System.out.print(".");
         }
 
-        System.out.println("Task done...");
+        logger.info("Task done!");
+        long end = System.currentTimeMillis();
+        long factorial = future.get();
 
-        long factorial = (long) future.get();
-        System.out.println("Factorial is equal to: " + factorial);
+        System.out.print("\n\n------> Factorial is equal to: " + factorial);
+        System.out.print("\n\nProcess took: ------> " + (end-start) + " ms!");
+
         threadPool.shutdown();
         input.close();
 
     }
 
-    private static class Factorial implements Callable<Long> {
-
-        private final int number;
-
-        public Factorial(int number) {
-            this.number = number;
-        }
+    private record Factorial(Long number) implements Callable<Long> {
 
         @Override
-        public Long call() {
+            public Long call() {
 
-            long result = 0L;
-            try {
+                long result = 0L;
+                try {
 
-                result = calculateFactorial(number);
-            } catch (InterruptedException e) {
-                Logger.getLogger(App.class.getName())
-                .log(java.util.logging.Level.SEVERE, "logging", new Exception("Exception"));
+                    result = calculateFactorial(number);
+
+                } catch (InterruptedException e) {
+
+                    Logger.getLogger(App.class.getName())
+                            .log(java.util.logging.Level.SEVERE, "logging", new Exception("Exception"));
+                }
+
+                return result;
+
             }
 
-            return result;
+            private Long calculateFactorial(Long numberToBeFactored) throws InterruptedException {
 
+                if (numberToBeFactored <= 0) {
+                    throw new IllegalArgumentException("Number to be Factored must be greater than zero");
+                }
+
+                long factoredNumber = 1L;
+
+                while (numberToBeFactored > 0) {
+
+                    factoredNumber = numberToBeFactored * factoredNumber;
+                    numberToBeFactored--;
+                }
+
+                return factoredNumber;
+            }
         }
-
-        private Long calculateFactorial(int numberToBeFactored) throws InterruptedException {
-
-            if (numberToBeFactored <= 0) {
-                throw new IllegalArgumentException("Number to be Factored must be greater than zero");
-            }
-
-            Long factoredNumber = 1L;
-
-            while (numberToBeFactored > 0) {
-
-                factoredNumber = numberToBeFactored * factoredNumber;
-                numberToBeFactored--;
-            }
-            return factoredNumber;
-        }
-    }
 }
