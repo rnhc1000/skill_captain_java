a4ed079f821681f5b55eba731c39339270982cd8
Ricardo Ferreira <rnhc1000@hotmail.com>
2024-05-25 15:35:33 -0300

    fixed skill icons README.md

--- advanced/dayFour/FutureFactorial/src/main/java/br/dev/ferreiras/App.java
+++ advanced/dayFour/FutureFactorial/src/main/java/br/dev/ferreiras/App.java
@@ -13,74 +13,74 @@ import java.util.logging.Logger;
 
 public class App {
 
-    private static final ExecutorService threadPool = Executors.newFixedThreadPool(10);
+  private static final ExecutorService threadPool = Executors.newFixedThreadPool(10);
 
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
+  public static void main(String[] args) throws InterruptedException, ExecutionException {
 
-        Scanner input = new Scanner(System.in);
-        String logTimeDate = "dd.MM.yyyy HH:mm:ss.SSS";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(logTimeDate);
-        System.out.println(formatter.format(LocalDateTime.now()));
-        System.out.print("Input the number to be processed: ");
-        Long number = input.nextLong();
+    Scanner input = new Scanner(System.in);
+    String logTimeDate = "dd.MM.yyyy HH:mm:ss.SSS";
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern(logTimeDate);
+    System.out.println(formatter.format(LocalDateTime.now()));
+    System.out.print("Input the number to be processed: ");
+    Long number = input.nextLong();
 
-        Factorial numberToBeFactored = new Factorial(number);
-        Logger logger = Logger.getLogger(App.class.getName());
-        logger.info("Task had just been submitted!");
-        long start = System.currentTimeMillis();
-        Future<Long> future = threadPool.submit(numberToBeFactored);
+    Factorial numberToBeFactored = new Factorial(number);
+    Logger logger = Logger.getLogger(App.class.getName());
+    logger.info("Task had just been submitted!");
+    long start = System.currentTimeMillis();
+    Future<Long> future = threadPool.submit(numberToBeFactored);
 
-        while (!future.isDone()) {
-            System.out.print(".");
-        }
+    while (!future.isDone()) {
+      System.out.print(".");
+    }
 
-        logger.info("Task done!");
-        long end = System.currentTimeMillis();
-        long factorial = future.get();
+    logger.info("Task done!");
+    long end = System.currentTimeMillis();
+    long factorial = future.get();
 
-        System.out.print("\n\n------> Factorial is equal to: " + factorial);
-        System.out.print("\n\nProcess took: ------> " + (end-start) + " ms!");
+    System.out.print("\n\n------> Factorial is equal to: " + factorial);
+    System.out.print("\n\nProcess took: ------> " + (end - start) + " ms!");
 
-        threadPool.shutdown();
-        input.close();
+    threadPool.shutdown();
+    input.close();
+
+  }
+
+  private record Factorial(Long number) implements Callable<Long> {
+
+    @Override
+    public Long call() {
+
+      long result = 0L;
+      try {
+
+        result = calculateFactorial(number);
+
+      } catch (InterruptedException e) {
+
+        Logger.getLogger(App.class.getName())
+                .log(java.util.logging.Level.SEVERE, "logging", new Exception("Exception"));
+      }
+
+      return result;
 
     }
 
-    private record Factorial(Long number) implements Callable<Long> {
+    private Long calculateFactorial(Long numberToBeFactored) throws InterruptedException {
 
-        @Override
-            public Long call() {
+      if (numberToBeFactored <= 0) {
+        throw new IllegalArgumentException("Number to be Factored must be greater than zero");
+      }
 
-                long result = 0L;
-                try {
+      long factoredNumber = 1L;
 
-                    result = calculateFactorial(number);
+      while (numberToBeFactored > 0) {
 
-                } catch (InterruptedException e) {
+        factoredNumber = numberToBeFactored * factoredNumber;
+        numberToBeFactored--;
+      }
 
-                    Logger.getLogger(App.class.getName())
-                            .log(java.util.logging.Level.SEVERE, "logging", new Exception("Exception"));
-                }
-
-                return result;
-
-            }
-
-            private Long calculateFactorial(Long numberToBeFactored) throws InterruptedException {
-
-                if (numberToBeFactored <= 0) {
-                    throw new IllegalArgumentException("Number to be Factored must be greater than zero");
-                }
-
-                long factoredNumber = 1L;
-
-                while (numberToBeFactored > 0) {
-
-                    factoredNumber = numberToBeFactored * factoredNumber;
-                    numberToBeFactored--;
-                }
-
-                return factoredNumber;
-            }
-        }
+      return factoredNumber;
+    }
+  }
 }
--- advanced/dayThree/Executor.java
+++ advanced/dayThree/Executor.java
@@ -14,83 +14,78 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- * 
  * Create a program that reads a directory containing a large number of files
- * and
- * performs word count on each file using a thread pool of size 10.
+ * and performs word count on each file using a thread pool of size 10.
  * The program should use an ExecutorService to manage the thread pool and
- * submit tasks
- * for each file to be processed concurrently.
+ * submit tasks for each file to be processed concurrently.
  * Once all the files have been processed, the program should output the total
- * word count
- * for all the files.
- * 
+ * word count for all the files.
  */
-public  class Executor {
+public class Executor {
 
-    private synchronized void readDirectoryThreaded() {
-        int threadPool = 10;
-        ExecutorService executorService = Executors.newFixedThreadPool(threadPool);
+  private synchronized void readDirectoryThreaded() {
+    int threadPool = 10;
+    ExecutorService executorService = Executors.newFixedThreadPool(threadPool);
 
-        ReadDirectory readDirectory = new ReadDirectory();
+    ReadDirectory readDirectory = new ReadDirectory();
 
-        //for (int i = 0; i < threadPool; i++) {
-           executorService.submit(readDirectory);
-            // try {
-            //     System.out.println(result.get());
-            // } catch (InterruptedException | ExecutionException e) {
+    //for (int i = 0; i < threadPool; i++) {
+    executorService.submit(readDirectory);
+    // try {
+    //     System.out.println(result.get());
+    // } catch (InterruptedException | ExecutionException e) {
 
-            //     e.printStackTrace();
+    //     e.printStackTrace();
 
-            // }
-        //}
-        executorService.shutdown();
+    // }
+    //}
+    executorService.shutdown();
 
+  }
+
+  class ReadDirectory implements Runnable {
+
+    private String name = "";
+
+    ReadDirectory(String name) {
+      this.name = name;
     }
 
-    class ReadDirectory implements Runnable {
-
-        private String name = "";
-
-        ReadDirectory(String name) {
-
-            this.name = name;
-        }
-
-        ReadDirectory() {
-
-            
-        }
-
-        @Override
-        public void run() {
-
-            String dir = "./advanced/dayThree/";
-            Set<String> listOfFiles = new HashSet<>();
-
-                try (Stream<Path> stream = Files.list(Paths.get(dir))) {
-                     listOfFiles = stream
-                      .filter(file -> !Files.isDirectory(file))
-                      .map(Path::getFileName)
-                      .map(Path::toString)
-                      .collect(Collectors.toSet());
-                } catch (IOException e) {
-                    System.out.println("Error...");
-                    e.printStackTrace();
-                }
-                System.out.println("Reading directory....");
-                System.out.println(listOfFiles);
-
-            }
-
-        }
-
-    
-
-    public static void main(String... args){
-
-        Executor executor = new Executor();
-        
-        executor.readDirectoryThreaded();
+    ReadDirectory() {
     }
+
+    @Override
+    public void run() {
+
+      String dir = "./advanced/dayThree/";
+      Set<String> listOfFiles = new HashSet<>();
+
+      try (Stream<Path> stream = Files.list(Paths.get(dir))) {
+
+        listOfFiles = stream
+                .filter(file -> !Files.isDirectory(file))
+                .map(Path::getFileName)
+                .map(Path::toString)
+                .collect(Collectors.toSet());
+
+      } catch (IOException e) {
+
+        System.out.println("Error...");
+        e.printStackTrace();
+
+      }
+
+      System.out.println("Reading directory....");
+      System.out.println(listOfFiles);
+
+    }
+  }
+
+
+  public static void main(String... args) {
+
+    Executor executor = new Executor();
+    executor.readDirectoryThreaded();
+
+  }
 }
